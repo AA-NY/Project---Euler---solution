c928f6e43916facf4ea9036f1375d50a2230d629
Asra <136901385+AA-NY@users.noreply.github.com>
2023-09-23 20:21:41 +0330

    Update Promlem 10.py

--- Promlem 10.py
+++ Promlem 10.py
@@ -1,23 +1,16 @@
-def sum_of_primes(n):
-    """
-    Returns the sum of all primes below n.
-    """
-    # Initialize a boolean array to mark all numbers as prime
-    is_prime = [True] * n
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-    # 0 and 1 are not prime
-    is_prime[0] = is_prime[1] = False
+sum_of_primes = 0
+for i in range(2, 2000000):
+    if is_prime(i):
+        sum_of_primes += i
 
-    # Sieve of Eratosthenes algorithm to mark non-prime numbers
-    for i in range(2, int(n ** 0.5) + 1):
-        if is_prime[i]:
-            for j in range(i ** 2, n, i):
-                is_prime[j] = False
+print(sum_of_primes)
 
-    # Sum all prime numbers
-    return sum(i for i in range(n) if is_prime[i])
-
-# Test the function with n = 2000000
-print(sum_of_primes(2000000))  
-
-# Output: 142913828922
+#the output = 142913828922
